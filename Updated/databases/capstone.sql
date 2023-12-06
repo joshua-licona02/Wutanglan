a555d92488a9542e121293c8586c6ddc76c1b5de
codingKeydet <johnstonjr24@mail.vmi.edu>
2023-12-06 11:18:10 -0500

    Updated Database

--- Updated/databases/capstone.sql
+++ Updated/databases/capstone.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 06, 2023 at 02:35 AM
+-- Generation Time: Dec 06, 2023 at 05:17 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -24,11 +24,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `accountability`
+--
+
+CREATE TABLE `accountability` (
+  `accountability_id` int(11) NOT NULL,
+  `date` varchar(255) NOT NULL,
+  `time` varchar(255) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `cadet_id` varchar(11) NOT NULL,
+  `status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `cadets`
 --
 
 CREATE TABLE `cadets` (
-  `id_number` varchar(255) NOT NULL,
+  `id_number` varchar(11) NOT NULL,
   `first_name` varchar(255) NOT NULL,
   `last_name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
@@ -66,6 +81,41 @@ CREATE TABLE `commstaff` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `courses`
+--
+
+CREATE TABLE `courses` (
+  `course_id` int(11) NOT NULL,
+  `course_title` int(11) NOT NULL,
+  `course_code` int(11) NOT NULL,
+  `section` int(11) NOT NULL,
+  `section_time` int(11) NOT NULL,
+  `date` varchar(255) NOT NULL,
+  `professor_id` int(11) NOT NULL,
+  `building` varchar(255) NOT NULL,
+  `classroom` int(11) NOT NULL,
+  `department` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `professor`
+--
+
+CREATE TABLE `professor` (
+  `professor_id` int(11) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `department` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `title/rank` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `secretary`
 --
 
@@ -83,6 +133,14 @@ CREATE TABLE `secretary` (
 --
 
 --
+-- Indexes for table `accountability`
+--
+ALTER TABLE `accountability`
+  ADD PRIMARY KEY (`accountability_id`),
+  ADD UNIQUE KEY `cadet_id` (`cadet_id`),
+  ADD KEY `course_id` (`course_id`);
+
+--
 -- Indexes for table `cadets`
 --
 ALTER TABLE `cadets`
@@ -95,10 +153,56 @@ ALTER TABLE `commstaff`
   ADD PRIMARY KEY (`id_number`);
 
 --
+-- Indexes for table `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`course_id`),
+  ADD KEY `professor_id` (`professor_id`);
+
+--
+-- Indexes for table `professor`
+--
+ALTER TABLE `professor`
+  ADD PRIMARY KEY (`professor_id`);
+
+--
 -- Indexes for table `secretary`
 --
 ALTER TABLE `secretary`
   ADD PRIMARY KEY (`id_number`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `accountability`
+--
+ALTER TABLE `accountability`
+  MODIFY `accountability_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `professor`
+--
+ALTER TABLE `professor`
+  MODIFY `professor_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `accountability`
+--
+ALTER TABLE `accountability`
+  ADD CONSTRAINT `accountability_ibfk_1` FOREIGN KEY (`cadet_id`) REFERENCES `cadets` (`id_number`),
+  ADD CONSTRAINT `accountability_ibfk_2` FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`);
+
+--
+-- Constraints for table `courses`
+--
+ALTER TABLE `courses`
+  ADD CONSTRAINT `courses_ibfk_1` FOREIGN KEY (`professor_id`) REFERENCES `professor` (`professor_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
