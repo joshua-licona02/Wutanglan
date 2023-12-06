c898260737813507c7de4c157028a90cf0f64dc5
RazorbackHillbilly <150824666+RazorbackHillbilly@users.noreply.github.com>
2023-12-05 23:28:22 -0500

    updated professor columns

--- Updated/databases/professor.sql
+++ Updated/databases/professor.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 06, 2023 at 05:18 AM
+-- Generation Time: Dec 06, 2023 at 05:27 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -33,7 +33,8 @@ CREATE TABLE `professor` (
   `email` varchar(255) NOT NULL,
   `professor_id` int(11) NOT NULL,
   `department` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `title/rank` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
