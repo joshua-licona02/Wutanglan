e59a39e1853954296cc2ba852472e39eaf1f1578
codingKeydet <johnstonjr24@mail.vmi.edu>
2023-12-11 08:47:08 -0500

    Updated Database

--- databases/capstone.sql
+++ databases/capstone.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 08, 2023 at 02:33 AM
+-- Generation Time: Dec 11, 2023 at 02:46 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -20,6 +20,20 @@ SET time_zone = "+00:00";
 --
 -- Database: `capstone`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `431_full`
+-- (See below for the actual view)
+--
+CREATE TABLE `431_full` (
+`id_number` varchar(11)
+,`last_name` varchar(255)
+,`rank` varchar(255)
+,`course_id` int(255)
+,`section_marcher` int(255)
+);
 
 -- --------------------------------------------------------
 
@@ -64,7 +78,8 @@ INSERT INTO `cadets` (`id_number`, `first_name`, `last_name`, `email`, `class`, 
 ('0619046', 'Josh', 'Licona', 'liconajr24@vmi.edu', 2024, 'PVT', 'CIS', 'password', 'Company'),
 ('0655502', 'Rachel', 'Greathouse', 'greathousere25@vmi.edu', 2025, 'PVT', 'EC', 'password', 'Company'),
 ('10675729', 'Jacob', 'Hill', 'hillja24@vmi.edu', 2024, '1LT', 'CIS', 'password', 'Staff'),
-('1234543', 'Mark', 'Shelton', 'sheltonml24@vmi.edu', 2024, '1CPT', 'CIS', 'password', 'Staff');
+('1234543', 'Mark', 'Shelton', 'sheltonml24@vmi.edu', 2024, '1CPT', 'CIS', 'password', 'Staff'),
+('12346782', 'Sam', 'Patterson', 'pattersonsb24@vmi.edu', 2024, '2LT', 'CE', 'password', 'Company');
 
 -- --------------------------------------------------------
 
@@ -98,7 +113,9 @@ CREATE TABLE `courses` (
   `course_title` varchar(255) NOT NULL,
   `course_code` varchar(255) NOT NULL,
   `section` int(11) NOT NULL,
+  `section_day` varchar(255) NOT NULL,
   `section_time` time NOT NULL,
+  `section_end` time NOT NULL,
   `date_start` date NOT NULL,
   `date_end` date NOT NULL,
   `professor_id` int(11) NOT NULL,
@@ -111,9 +128,12 @@ CREATE TABLE `courses` (
 -- Dumping data for table `courses`
 --
 
-INSERT INTO `courses` (`course_id`, `course_title`, `course_code`, `section`, `section_time`, `date_start`, `date_end`, `professor_id`, `building`, `classroom`, `department`) VALUES
-(1, 'Pre-Capstone', '480', 2, '09:25:00', '2023-08-29', '2023-12-14', 1, 'Mallory Hall', 318, 'CIS'),
-(2, 'Pre-Capstone', '480', 1, '08:00:00', '2023-08-29', '2023-12-14', 1, 'Mallory Hall', 318, 'CIS');
+INSERT INTO `courses` (`course_id`, `course_title`, `course_code`, `section`, `section_day`, `section_time`, `section_end`, `date_start`, `date_end`, `professor_id`, `building`, `classroom`, `department`) VALUES
+(1, 'Pre-Capstone', '480', 2, 'TR', '09:25:00', '10:40:00', '2023-08-29', '2023-12-14', 1, 'Mallory Hall', 318, 'CIS'),
+(2, 'Pre-Capstone', '480', 1, 'TR', '08:00:00', '09:15:00', '2023-08-29', '2023-12-14', 1, 'Mallory Hall', 318, 'CIS'),
+(3, 'Information Organization/Management', '431', 1, 'MWF', '13:00:00', '13:50:00', '2023-08-29', '2023-12-14', 2, 'Mallory Hall', 211, 'CIS'),
+(4, 'Introduction to Computer Science', '101', 1, 'MWF', '10:00:00', '10:50:00', '2023-08-29', '2023-12-14', 3, 'Mallory Hall', 314, 'CIS'),
+(5, 'Introduction to Computer Science', '101', 2, 'MWF', '10:00:00', '10:50:00', '2023-08-29', '2023-12-14', 4, 'Mallory Hall', 314, 'CIS');
 
 -- --------------------------------------------------------
 
@@ -124,17 +144,23 @@ INSERT INTO `courses` (`course_id`, `course_title`, `course_code`, `section`, `s
 CREATE TABLE `course_enrollment` (
   `enrollment_id` int(255) NOT NULL,
   `cadet_id` varchar(255) NOT NULL,
-  `cadet_lastname` varchar(255) NOT NULL,
   `course_id` int(255) NOT NULL,
-  `section_marcher` int(255) NOT NULL DEFAULT 0
+  `section_marcher` int(255) NOT NULL DEFAULT 0,
+  `semester` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `course_enrollment`
 --
 
-INSERT INTO `course_enrollment` (`enrollment_id`, `cadet_id`, `cadet_lastname`, `course_id`, `section_marcher`) VALUES
-(1, '0609724', '', 1, 2);
+INSERT INTO `course_enrollment` (`enrollment_id`, `cadet_id`, `course_id`, `section_marcher`, `semester`) VALUES
+(1, '0609724', 1, 1, 'FL23'),
+(2, '0609724', 3, 2, 'FL23'),
+(3, '1234543', 3, 1, 'FL23'),
+(4, '0655502', 3, 0, 'FL23'),
+(5, '12346782', 4, 2, 'FL23'),
+(6, '0609724', 4, 1, 'FL23'),
+(7, '10675729', 3, 3, 'FL23');
 
 -- --------------------------------------------------------
 
@@ -149,15 +175,15 @@ CREATE TABLE `professor` (
   `email` varchar(255) NOT NULL,
   `department` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `title/rank` varchar(255) NOT NULL
+  `title` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `professor`
 --
 
-INSERT INTO `professor` (`professor_id`, `first_name`, `last_name`, `email`, `department`, `password`, `title/rank`) VALUES
-(1, 'Dennis', 'Gracanin', 'gracanind@vmi.edu', 'CIS', 'password', 'Dr.'),
+INSERT INTO `professor` (`professor_id`, `first_name`, `last_name`, `email`, `department`, `password`, `title`) VALUES
+(1, 'Denis', 'Gracanin', 'gracanind@vmi.edu', 'CIS', 'password', 'Dr.'),
 (2, 'Imran', 'Ghani', 'ghanii@vmi.edu', 'CIS', 'password', 'LTC'),
 (3, 'Doug', 'Wainwright', 'wainwrightdb@vmi.edu', 'CIS', 'password', 'MAJ'),
 (4, 'Ramoni', 'Lasisi', 'lasisiro@vmi.edu', 'CIS', 'password', 'LTC');
@@ -215,6 +241,15 @@ CREATE TABLE `secretary` (
 INSERT INTO `secretary` (`id_number`, `first_name`, `last_name`, `email`, `dept`, `password`) VALUES
 ('5402222', 'Sandra', 'Williams', 'williamssr@vmi.edu', 'CIS', 'password');
 
+-- --------------------------------------------------------
+
+--
+-- Structure for view `431_full`
+--
+DROP TABLE IF EXISTS `431_full`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `431_full`  AS SELECT `cadets`.`id_number` AS `id_number`, `cadets`.`last_name` AS `last_name`, `cadets`.`rank` AS `rank`, `course_enrollment`.`course_id` AS `course_id`, `course_enrollment`.`section_marcher` AS `section_marcher` FROM ((`cadets` join `course_enrollment` on(`cadets`.`id_number` = `course_enrollment`.`cadet_id`)) join `rank` on(`rank`.`rank` = `cadets`.`rank`)) WHERE `course_enrollment`.`course_id` = 3 ORDER BY `rank`.`rank_id` ASC, `cadets`.`class` ASC, `cadets`.`last_name` ASC ;
+
 --
 -- Indexes for dumped tables
 --
@@ -252,8 +287,7 @@ ALTER TABLE `courses`
 --
 ALTER TABLE `course_enrollment`
   ADD PRIMARY KEY (`enrollment_id`),
-  ADD UNIQUE KEY `cadet_id` (`cadet_id`,`course_id`),
-  ADD KEY `cadet_lastname` (`cadet_lastname`);
+  ADD UNIQUE KEY `cadet_id` (`cadet_id`,`course_id`);
 
 --
 -- Indexes for table `professor`
@@ -287,19 +321,19 @@ ALTER TABLE `accountability`
 -- AUTO_INCREMENT for table `courses`
 --
 ALTER TABLE `courses`
-  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `course_enrollment`
 --
 ALTER TABLE `course_enrollment`
-  MODIFY `enrollment_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `enrollment_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `professor`
 --
 ALTER TABLE `professor`
-  MODIFY `professor_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `professor_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
